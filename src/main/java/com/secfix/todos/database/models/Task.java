6c8abfc46f34d79d761ed1d8c842855558d367c7
Balaji C <f20180613h@alumni.bits-pilani.ac.in>
2024-12-19 11:13:12 +0530

    Fix: updated Task entity to handle long description text

--- src/main/java/com/secfix/todos/database/models/Task.java
+++ src/main/java/com/secfix/todos/database/models/Task.java
@@ -29,7 +29,7 @@ public class Task {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "description")
+    @Column(name = "description", columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
